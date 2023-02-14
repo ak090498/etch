fabc6cf0e37b7085d25dec945165095868d10ee8
knowansaritra <aritrakunda3@gmail.com>
2023-02-14 20:11:01 +0530

    added the drawing functionality using clicks

--- src.js
+++ src.js
@@ -1,19 +1,46 @@
 const cont = document.getElementById("container")
-
+//const gridItem = document.querySelector(".grid-item");
+const input = document.getElementById("input")
 let generateGrid=(n)=>{
-    cont.style.cssText='grid-template-columns:repeat(10,30px)';
-    for(let i=0;i<10;i++)
+    let pixel=400/n;
+    let pixelPercent=(pixel/400)*100
+    let percentString="";
+    for(let i=0;i<n;i++)
     {
-    for(let j=0;j<10;j++)
+        percentString=percentString+pixelPercent+"%"+" "
+    }
+    console.log(percentString)
+    cont.style.cssText='grid-template-columns:'+percentString;
+    for(let i=0;i<n;i++)
+    {
+    for(let j=0;j<n;j++)
     {
     let node=document.createElement("div")
     //console.log(node)
     node.classList.add("grid-item")
+    node.style.height=pixelPercent;
+    node.style.width=pixelPercent;
+    node.setAttribute("id",i+""+j)
+   
     cont.appendChild(node)
+    let gridItem = document.getElementById(i+""+j)
+
+    gridItem.addEventListener("click",()=>{
+        gridItem.style.backgroundColor="#00FF00"
+       
+    })
     }
     }
 }
 bt1.addEventListener("click",()=>{
-    generateGrid(10);
-
+    while(cont.firstChild)
+     {
+        cont.removeChild(cont.firstChild)
+     }
+     if(input.value>100)
+     input.value=100;
+    generateGrid(input.value);
+    
 })
+
+
